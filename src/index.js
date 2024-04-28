0ebb14a0ee78161bd6b10932800acaffada523a6
feanlau <feanlau@outlook.com>
2024-04-28 10:27:21 +0800

    这里采用了两种不同的实现方式

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import AppFunctional from './AppFunctional';
+// import AppFunctional from './AppFunctional';
+import App from './App';
 import './styles.css';
 
-ReactDOM.render(<AppFunctional />, document.getElementById('root'));
+// 这里采用了两种不同的实现方式
+// ReactDOM.render(<AppFunctional />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
