568d8037d89ea0446c6e0940bc76330e7cdec5c1
feanlau <feanlau@outlook.com>
2024-04-28 10:40:13 +0800

    docs : add .

--- src/index.js
+++ src/index.js
@@ -6,4 +6,5 @@ import './styles.css';
 
 // 这里采用了两种不同的实现方式
 // ReactDOM.render(<AppFunctional />, document.getElementById('root'));
+// 用useEffect的方式更加简单
 ReactDOM.render(<App />, document.getElementById('root'));
